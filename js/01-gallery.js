22be20cfdc0644a9290f67442e6165a1bddcfc42
Oleh Baramykin <olegix7891@ukr.net>
2022-12-07 17:24:14 +0200

    дз1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,46 +1,49 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-const galleryContainer = document.querySelector('.gallery');
-const galleryMarkup = createGalleryCardMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// const galleryContainer = document.querySelector('.gallery');
+// const galleryMarkup = createGalleryCardMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-function createGalleryCardMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
-    })
-    .join('');
-}
+// function createGalleryCardMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`;
+//     })
+//     .join('');
+// }
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
-function onGalleryContainerClick(evt) {
-  evt.preventDefault();
-  const isImage = evt.target.classList.contains('gallery__image');
-  if (!isImage) {
-    return;
-  }
+// galleryContainer.addEventListener('click', onGalleryContainerClick);
+// function onGalleryContainerClick(evt) {
+//   evt.preventDefault();
+//   const isImage = evt.target.classList.contains('gallery__image');
+//   if (!isImage) {
+//     return;
+//   }
 
-  const instance = basicLightbox.create(`
-        <div class="modal">
-            <img src="${evt.target.dataset.source}"/>
-        </div>
-    `);
-  instance.show();
+//   const instance = basicLightbox.create(`
+//         <div class="modal">
+//             <img src="${evt.target.dataset.source}"/>
+//         </div>
+//     `);
+//   instance.show();
 
-  document.addEventListener('keydown', closeModal);
-  function closeModal(e) {
-    if (e.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
+//   document.addEventListener('keydown', closeModal);
+//   function closeModal(e) {
+//     if (e.code === 'Escape') {
+//       instance.close();
+//     }
+//   }
+// }
+console.log('This message will not appear in the console');
+
+cos value = 5;
